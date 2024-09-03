7291badacdc721cdba96b0d6aa6912dcd83df0ee
wyhwong <wyhwong@link.cuhk.edu.hk>
2024-09-03 23:57:48 +0800

    Env

--- src/pipeline/utils/env.py
+++ src/pipeline/utils/env.py
@@ -2,12 +2,10 @@ import os
 import zoneinfo
 
 
+# For logging
 STREAMING_LOG_LEVEL = int(os.getenv("STREAMING_LOG_LEVEL", "30"))
 FILE_LOG_LEVEL = int(os.getenv("FILE_LOG_LEVEL", "10"))
 LOG_FILEPATH = os.getenv("LOG_FILEPATH", "./runtime.log")
-# NOTE: Although LOG_FMT and LOG_DATEFMT are in env.py, we do not expect
-#       them to be changed by environment variables. They define the logging
-#       style of the project and should not be changed.
-LOG_FMT = "%(asctime)s [%(name)s | %(levelname)s]: %(message)s"
-LOG_DATEFMT = "%Y-%m-%dT%H:%M:%SZ"
+LOG_FMT = os.getenv("LOG_FMT", "%(asctime)s [%(name)s | %(levelname)s]: %(message)s")
+LOG_DATEFMT = os.getenv("LOG_DATEFMT", "%Y-%m-%dT%H:%M:%SZ")
 TZ = zoneinfo.ZoneInfo(os.getenv("TZ") or "UTC")
